fbb04fa3185aaeab874b84568a3daf556028fa85
Soumita Basu <moon1237879@gmail.com>
2022-07-09 03:29:37 +0530

    Pic added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,22 @@
-import "./styles.css";
+import './styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
-    <div className="Heading">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className='section1 container-fluid p-0 vh-100'>
+      <div className="heading">
+      <div className='row'>
+        <div className='col logo ms-3 ms-lg-4 mb-2 mt-2'>Fanfic</div>
+        <div className='col'>
+          <div className='row'>
+            .<div className='col'>
+
+            </div>
+            <div className='col'></div>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
